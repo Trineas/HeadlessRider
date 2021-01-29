7dda66e653125113481be36dd63b577b7e824de0
Yoshinyong <zxasq9876@googlemail.com>
2021-01-29 14:13:03 +0100

    Added Missing files

---
+++ Assets/Script/MapController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+
+    private float spawnDelay;
+    private List<GameObject> mapAssets = new List<GameObject>();
+    private float spawnTimer = 0f;
+    private Vector3 nextSpawn = Vector3.zero;
+
+    void SpawnAsset()
+    {
+        int mapIdx = Random.Range(0, mapAssets.Count);
+        GameObject temp = Instantiate(mapAssets[mapIdx], nextSpawn, Quaternion.identity);
+
+        // Set next Spawn Position;
+        Renderer mapBound = temp.GetComponent<Renderer>();
+        nextSpawn -= Vector3.forward * (mapBound.bounds.extents.z * 2f);
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        PlayerController p_Controller = player.GetComponent<PlayerController>();
+
+        spawnDelay = p_Controller.ScrollSpeed * 3f;
+
+        foreach (Transform child in transform)
+        {
+            if(nextSpawn.z > child.position.z)
+            {
+                nextSpawn = child.position;
+
+                Renderer mapBound = child.GetComponent<Renderer>();
+                nextSpawn -= Vector3.forward * (mapBound.bounds.extents.z *2f);
+            }
+            mapAssets.Add(child.gameObject);
+        }
+        
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= spawnDelay)
+        {
+            SpawnAsset();
+            spawnTimer = 0;
+        }
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float ScrollSpeed;
+    public float MovementSpeed;
+
+    private float mov_x;
+    private float mov_y;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ScrollSpeed = ScrollSpeed == 0 ? 0.3f : ScrollSpeed;
+        MovementSpeed = MovementSpeed == 0 ? 3f : MovementSpeed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += -Vector3.forward * ScrollSpeed;
+        UpdateMovement();
+        
+    }
+
+    void UpdateMovement()
+    {
+        mov_x = Input.GetAxis("Horizontal");
+        //mov_y = Input.GetAxis("Vertical");
+
+        transform.position += Vector3.right * mov_x * MovementSpeed * Time.deltaTime;
+        //transform.position += Vector3.forward * mov_y * Time.deltaTime;
+
+
+    }
+}
