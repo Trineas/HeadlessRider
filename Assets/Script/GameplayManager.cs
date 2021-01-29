b936d6ff0716f4d6a87bfbe1a6a21665b9383304
Yoshinyong <zxasq9876@googlemail.com>
2021-01-29 21:00:52 +0100

    Game drops Obstacles now

--- Assets/Script/GameplayManager.cs
+++ Assets/Script/GameplayManager.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class GameplayManager : MonoBehaviour
 {
     public float ScrollSpeed;
+    [Range(3,9)] public int delayMin;
+    [Range(5,12)] public int delayMax;
+    public List<GameObject> obstacles;
+
+    private float obstacleLTimer = 0f;
+    private float obstacleJRTimer = 0f;
+
+    private float obstacleLDelay = 3f;
+    private float obstacleJRDelay = 8f;
+
+    private List<GameObject> obstaclesL = new List<GameObject>();
+    private List<GameObject> obstaclesJR = new List<GameObject>();
 
     private GameObject DarkWall;
-
-    public static GameplayManager Instance { get; private set; }
 
     // Start is called before the first frame update
     void Awake()
@@ -16,13 +26,19 @@ public class GameplayManager : MonoBehaviour
         ScrollSpeed = ScrollSpeed == 0 ? 0.3f : ScrollSpeed;
         DarkWall = GameObject.FindGameObjectWithTag("DeathTrigger");
 
-        if (Instance != null && Instance != this)
+        delayMax = delayMin > delayMax ? delayMin + 1 : delayMax;
+
+        foreach(GameObject obst in obstacles)
         {
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            Instance = this;
+            if(obst.tag == "Lying Obstacle")
+            {
+                obstaclesL.Add(obst);
+            }
+
+            if(obst.tag == "Jumping Obstacle" || obst.tag == "Rolling Obstacle")
+            {
+                obstaclesJR.Add(obst);
+            }
         }
     }
 
@@ -30,5 +46,40 @@ public class GameplayManager : MonoBehaviour
     void FixedUpdate()
     {
         DarkWall.transform.position -= Vector3.forward * ScrollSpeed;
+
+        obstacleLTimer += Time.deltaTime;
+        obstacleJRTimer += Time.deltaTime;
+
+        if (obstacleLTimer >= obstacleLDelay)
+        {
+            SpawnObstacle("L");
+            obstacleLTimer = 0f;
+            obstacleLDelay = 1.5f;
+        }
+
+        if (obstacleJRTimer >= obstacleJRDelay)
+        {
+            SpawnObstacle("JR");
+            obstacleJRTimer = 0f;
+            obstacleJRDelay = 2f;
+        }
+    }
+
+    void SpawnObstacle(string mode)
+    {
+        if(mode == "L")
+        {
+            Vector3 cameraPos = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+            int obstIdx = Random.Range(0, obstaclesL.Count);
+            Instantiate(obstaclesL[obstIdx], cameraPos - Vector3.down * 3f, obstaclesL[obstIdx].transform.rotation);
+        }
+
+        if(mode == "JR")
+        {
+            Vector3 cameraPos = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+            int obstIdx = Random.Range(0, obstaclesJR.Count);
+            Instantiate(obstaclesJR[obstIdx], cameraPos - Vector3.down * 3f, obstaclesJR[obstIdx].transform.rotation);
+        }
+        
     }
 }
---
+++ Assets/Script/ObstacleController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    public float BouncePower = 10f;
+    public float FallSpeed = 5f;
+
+    private float ScrollSpeed;
+    private float JumpPower = 3f;
+    private enum obstType { Jumping, Rolling, Lying };
+    private Renderer currentTrack;
+
+    
+
+
+    [SerializeField] obstType ObstacleType = obstType.Jumping;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "DeathTrigger")
+        {
+            StartCoroutine(DestoryYourself());
+        }
+
+        if(ObstacleType == obstType.Jumping && collision.gameObject.tag == "RunTrack")
+        {
+            JumpPower = BouncePower;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ScrollSpeed = GameObject.Find("GameplayManager").GetComponent<GameplayManager>().ScrollSpeed;
+
+        //decide Spawn point
+        transform.position = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+
+        RaycastHit trackLoc;
+        if (Physics.Raycast(transform.position, Vector3.down, out trackLoc, transform.position.y + 20f))
+        {
+            currentTrack = trackLoc.transform.GetComponent<Renderer>();
+        }
+
+        float offsetSide = currentTrack.bounds.extents.x - 4f;
+        float VertPos = Random.Range(-offsetSide, offsetSide);
+        transform.position += Vector3.right * VertPos;
+
+        if(ObstacleType == obstType.Lying)
+        {
+            Renderer thisObj = GetComponent<Renderer>();
+            transform.position += Vector3.up * (trackLoc.transform.position.y - transform.position.y + thisObj.bounds.extents.y);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position += Vector3.forward * ScrollSpeed * Time.deltaTime;
+        if(ObstacleType == obstType.Jumping || ObstacleType == obstType.Rolling)
+        {
+            transform.position += Vector3.up * JumpPower * Time.deltaTime;
+            JumpPower -= Time.deltaTime * FallSpeed;
+        }
+    }
+
+    IEnumerator DestoryYourself()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+}
